e16e5725d01ae12585af057acbe6bcd2a9091559
dnksebastian <sebastian.dulniawka@gmail.com>
2023-07-21 18:51:26 +0200

    graphql 8.9

---
+++ part8/libraryapp-frontend/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+query {
+  allAuthors {
+    name
+    born
+    bookCount
+  }
+}
+`
+
+const ALL_BOOKS = gql`
+query {
+  allBooks {
+    title
+    published
+    author
+    genres
+  }
+}
+`
+
+const App = () => {
+  const [page, setPage] = useState('authors')
+
+  const authorsResult = useQuery(ALL_AUTHORS)
+  const booksResult = useQuery(ALL_BOOKS)
+
+  if (authorsResult.loading || booksResult.loading) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors show={page === 'authors'} allauthors={authorsResult.data.allAuthors} />
+
+      <Books show={page === 'books'} allbooks={booksResult.data.allBooks}/>
+
+      <NewBook show={page === 'add'} />
+    </div>
+  )
+}
+
+export default App
